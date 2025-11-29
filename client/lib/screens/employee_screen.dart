236c3ad2473026bb29fb4604109ce6e0d25bf915
Alvi Choirinnikmah <avialvi08@gmail.com>
2025-11-29 20:54:01 +0700

    fix: fix UI issues and resolve merge conflicts

--- client/lib/screens/employee_screen.dart
+++ client/lib/screens/employee_screen.dart
@@ -5,21 +5,19 @@ import 'package:client/widgets/custom_card.dart';
 class EmployeeScreen extends StatelessWidget {
   final List<Map<String, String>> employees;
 
-  const EmployeeScreen({
-    super.key,
-    this.employees = const [],
-  });
-  
+  const EmployeeScreen({super.key, this.employees = const []});
+
   @override
   Widget build(BuildContext context) {
-    final data = employees.isNotEmpty ? employees :
-    List.generate(
-      6,
-      (index) => {
-        "name": "Andi Budi Carmen",
-        "role": "Front-End Developer",
-      },
-    );
+    final data = employees.isNotEmpty
+        ? employees
+        : List.generate(
+            6,
+            (index) => {
+              "name": "Andi Budi Carmen",
+              "role": "Front-End Developer",
+            },
+          );
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -70,17 +68,29 @@ class EmployeeScreen extends StatelessWidget {
 
             Expanded(
               child: ListView.separated(
-                itemCount: employees.length,
+                itemCount: data.length,
                 separatorBuilder: (_, __) => const SizedBox(height: 12),
                 itemBuilder: (context, index) {
                   return CustomCard(
                     name: data[index]['name']!,
                     position: data[index]['role']!,
                     onTap: () {},
+
+                    // Opsional: ganti icon action jadi icon edit
+                    actionIcon: Icons.edit,
+
+                    // Tambahkan ini supaya ikon muncul
+                    onInfoTap: () {
+                      print("Info diklik untuk ${data[index]['name']}");
+                    },
+                    onActionTap: () {
+                      print("Edit diklik untuk ${data[index]['name']}");
+                    },
+
                   );
                 },
               ),
-            )
+            ),
           ],
         ),
       ),
