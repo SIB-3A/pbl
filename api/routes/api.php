67e2f1c8788c6d73b892ea7434a9ea43cf7626d4
hmdnu <daniuyan71@gmail.com>
2025-12-06 16:43:57 +0700

    feat: Add API endpoint and UI for user payroll
    A new API endpoint `/position/{userId}` is introduced to fetch an
    employee's position and associated payroll rates. The client's
    `PayrollScreen` is updated to dynamically retrieve and display this
    data. Existing API routes for `/positions` and `/departements` are now
    protected by authentication.

--- api/routes/api.php
+++ api/routes/api.php
@@ -13,11 +13,9 @@ Route::get("/user/{id}", [UserController::class, "show_user"])->middleware(
 Route::get("/users", [UserController::class, "show_users"])->middleware(
     "auth:sanctum",
 );
-Route::get("/departements", [
-    DepartementController::class,
-    "show_departements",
-])->middleware("auth:sanctum");
-Route::get("/positions", [PositionController::class, "show_positions"]);
+Route::get("/departements", [DepartementController::class,"show_departements",])->middleware("auth:sanctum");
+Route::get("/positions", [PositionController::class, "show_positions"])->middleware("auth:sanctum");
+Route::get("/position/{userId}", [PositionController::class, "show_position"])->middleware("auth:sanctum");
 
 Route::post("/login", [AuthController::class, "login"]);
 Route::post("/register", [AuthController::class, "register"])->middleware(
